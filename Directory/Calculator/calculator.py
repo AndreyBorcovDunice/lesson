dd448b92113ce8b95d3ffce9d3678642efd97809
AndreyBorcovDunice <a.borcov@tg.dunice.net>
2025-09-05 15:00:23 +0300

    Add files via upload

---
+++ Directory/Calculator/calculator.py
@@ -0,0 +1,49 @@
+''' Module  '''
+import json
+import os
+from datetime import datetime
+
+def calculation():
+    ''' calculate functions '''
+    while True:
+        try:
+            expression = input("Введи выражение: ").lower()
+            if expression != "exit":
+                num1, operation, num2 = expression.split()
+                num1 = float(num1)
+                num2 = float(num2)
+                match operation:
+                    case "+":
+                        result = num1 + num2
+                    case "-":
+                        result = num1 - num2
+                    case "*":
+                        result = num1 * num2
+                    case "/":
+                        result = num1 / num2
+                    case "**":
+                        result = num1 ** num2
+                    case "%":
+                        result = num1 % num2
+                    case "//":
+                        result = num1 // num2
+                print(result)
+                dat = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                result = {result, expression, dat}
+                new_entry = result
+                file_path = "/home/dunice/Directory/Calculator/calc.json"
+                if os.path.exists(file_path):
+                    with open(file_path, "r", encoding="utf-8") as file:
+                        history = json.load(file)
+                else:
+                    history = []
+                history.append(new_entry)
+                with open (file_path, "w", encoding="utf-8") as file:
+                    json.dump(history, file, ensure_ascii=False, indent=4)
+            else:
+                exit(0)
+        except Exception as e:
+            print(f'Произошла ошибка: {e}')
+            continue
+
+calculation()
